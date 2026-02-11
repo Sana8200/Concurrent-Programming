7ce714fd34057339a60d8a5a6aeeb41d6a754950
Sana Monhaseri <sanamo@kth.se>
2026-02-11 19:05:34 +0100

    feat: one producer mutliple consumers problem using semaphores

--- homework-3-semaphores/hungry_birds.c
+++ homework-3-semaphores/hungry_birds.c
@@ -1,3 +1,8 @@
+/* Hungry Birds Problem (One Producer, Multiple Consumers)
+
+   gcc -o hungry_birds hungry_birds.c -lpthread
+   ./hungry_birds <worms> <babies>
+*/
 #ifndef _REENTRANT
 #define _REENTRANT
 #endif
@@ -5,17 +10,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>    // for sleep()
 
 #define MAXWORMS 20
-#define MAXBABYBIRDS 30      /* maximum number of baby birds(threads) */
+#define MAXBABYBIRDS 30    
 
-sem_t dish;
-sem_t empty, full;
-sem_t turn[MAXBABYBIRDS];
+sem_t dish;                // Mutex for common dish access
+sem_t empty;               // signal: dish is empty
+sem_t full;                // signal: dish is refilled
+sem_t turn[MAXBABYBIRDS];  // Array for round-robin ordering
 
-int attr;
-int worms;
-int W;       // number of worms in the dish
+int worms;          // current worms in dish
+int W;              // dish maximum size (capacity of dish)
 int numbBabyBirds;
 
 void *ParentBird(void *arg);
@@ -23,7 +29,7 @@ void *BabyBirds(void *arg);
 
 
 int main(int argc, char *argv[]){
-    setbuf(stdout, NULL);     // disables output buffering
+    //setbuf(stdout, NULL);     // disables output buffering, in case if we print without \n
     
     pthread_t babyBirds[MAXBABYBIRDS];
     pthread_t parentBird;
@@ -40,12 +46,14 @@ int main(int argc, char *argv[]){
     sem_init(&dish, 0, 1);     // mutex, init 1 (binary semaphore)
     sem_init(&empty, 0, 0);    // signaling, init 0 (signaling, coordinator semaphore)
     sem_init(&full, 0, 0);     // signaling, init 0 
+    // Initialize turns: Bird 0 goes first, others wait
     for (int i = 0; i < numbBabyBirds; i++) {
         sem_init(&turn[i], 0, (i == 0) ? 1 : 0);
     }
 
     // Creating threads (parentbird 1 thread, baby birds N threads)
-    pthread_create(&parentBird, NULL, ParentBird, NULL);      
+    pthread_create(&parentBird, NULL, ParentBird, NULL);    
+    printf("There are %d Baby Birds eating and sleeping\n", numbBabyBirds);  
     for(int i = 0; i < numbBabyBirds; i ++){
         pthread_create(&babyBirds[i], NULL, BabyBirds, (void *)(long) i);
     }
@@ -62,10 +70,10 @@ int main(int argc, char *argv[]){
 void *ParentBird(void *arg){
     printf("Parent Bird provided %d worms in the common dish for %d baby birds.\n", W, numbBabyBirds);
     while(1){
-        sem_wait(&empty);
-        worms = W;    // fill the dish 
-        printf("filled the dish with fresh %d worms!!!\n", W);
-        sem_post(&full);
+        sem_wait(&empty);       // Block until dish is empty 
+        worms = W;              // refill 
+        printf("Filled the dish with fresh %d worms!!!\n", W);    
+        sem_post(&full);       // signal waiting baby bird
     }
 }
 
@@ -73,8 +81,7 @@ void *ParentBird(void *arg){
 // Multiple Consumers (Baby Birds)
 void *BabyBirds(void *arg){
     int id = (int)(long)arg;
-
-    printf(" %d Baby Birds are starting to eat and sleep\n", numbBabyBirds);
+    int next = (id + 1) % numbBabyBirds;  // Next bird in circle
     while(1){
         sem_wait(&turn[id]);      // wait for my turn 
         sem_wait(&dish);          // enter CS
@@ -84,9 +91,9 @@ void *BabyBirds(void *arg){
             sem_wait(&full);      // wait for the dish to be full by parent (producer)
         }
         worms--;
-        printf("Baby bird %d ate a worm. Worms left: %d\n", id, worms);
-        sem_post(&dish);          // leave critical section 
-        sem_post(&turn[(id + 1) % numbBabyBirds]);    // signal next bird
+        printf("Baby %d ate. Remaining: %d\n", id, worms);
+        sem_post(&dish);          // leave critical section, unlock
+        sem_post(&turn[next]);    // signal next bird
         sleep(rand() % 3 + 1);    // sleep for random time, +1 so birds are always sleep for at least 1 second
     } 
 }
